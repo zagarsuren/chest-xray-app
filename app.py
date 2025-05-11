b720c13cac63978d26d1d0b0061d05380a55e1a0
zagarsuren <zagarsuren@gmail.com>
2025-05-11 17:26:48 +1000

    add new models

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+import tempfile
 
 # import predictors
 from predictors.swin_s import predictor as swin_s_predictor, CLASS_NAMES as SWIN_S_CLASSES
@@ -8,6 +9,12 @@ from predictors.swin_b import predictor as swin_b_predictor, CLASS_NAMES as SWIN
 from predictors.densenet import predictor as densenet_predictor, CLASSES as DENSENET_CLASSES
 from predictors.efficientnet import predictor as efficientnet_predictor, CLASSES as EFFICIENTNET_CLASSES
 from predictors.resnet import predictor as resnet_predictor, CLASSES as RESNET_CLASSES
+from predictors.inception import predictor as inception_predictor, CLASSES as INCEPTION_CLASSES
+from predictors.yolov11s import predictor as yolo_predictor, EXPECTED_CLASSES as YOLO_CLASSES
+from ensemble import EnsembleModelClassifier
+
+ENESEMBLE_CLASSES = ['Atelectasis', 'Cardiomegaly', 'Effusion', 'Nodule', 'Pneumothorax']
+
 
 st.set_page_config(page_title="Chest X-ray Classification", layout="centered")
 
@@ -15,7 +22,7 @@ st.set_page_config(page_title="Chest X-ray Classification", layout="centered")
 st.title("Chest X-ray Classification System")
 
 # --- MODEL SELECTION ---
-model_options = ["Swin-S", "Swin-B", "DenseNet121", "EfficientNetB0", "ResNet50"]
+model_options = ["Swin-B", "DenseNet121", "EfficientNetB0", "ResNet50", "InceptionV3", "YOLOv11s", "Ensemble"]
 model_choice = st.selectbox("Choose a model for classification", model_options)
 
 # --- FILE UPLOADER ---
@@ -30,6 +37,11 @@ if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
+    # Save to a temporary file if image_path is needed
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp_file:
+        image.save(tmp_file.name)
+        image_path = tmp_file.name  
+    # st.write(f"Image saved to: {image_path}")
     # True label selection     
     true_labels = {
         "Atelectasis": "Atelectasis",
@@ -63,6 +75,17 @@ if uploaded_file:
             elif model_choice == "ResNet50":
                 preds = resnet_predictor(img_arr)
                 classes = RESNET_CLASSES
+            elif model_choice == "InceptionV3":
+                preds = inception_predictor(img_arr)
+                classes = INCEPTION_CLASSES
+            elif model_choice == "YOLOv11s":
+                preds = yolo_predictor(img_arr)
+                classes = YOLO_CLASSES
+            elif model_choice == "Ensemble":
+                ensemble = EnsembleModelClassifier()
+                preds, predicted_class = ensemble.predict(image_path, method="voting")
+                classes = ENESEMBLE_CLASSES
+
             else:
                 st.error("Model not implemented.")
                 st.stop()
